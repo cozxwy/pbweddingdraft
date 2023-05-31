5cd6a76edafa788407b39edb907a760c7884655b
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-05-31 17:10:22 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        'my_bg_image' : "url('https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80')",
+      },
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/forms')
   ],
+  
 }
