b0700ca6ce04e2df4c8d7723c0268617bb98e5a8
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-05-31 22:34:50 +0700

    Add files via upload

--- pages/location/index.js
+++ pages/location/index.js
@@ -9,7 +9,7 @@ function index(){
   return (
     <>
 
-<main classNames="bg-gray-900 min-h-screen ">
+<main className="bg-gray-900 min-h-screen ">
     <Navbar />
     <div>
     <br></br>   <br></br>   <br></br>   <br></br>   <br></br>
