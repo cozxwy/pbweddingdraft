eba0e1c7fc926d58e193d1e9fd8f983f11750942
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-06-02 16:59:40 +0700

    Add files via upload

--- pages/location/index.js
+++ pages/location/index.js
@@ -32,9 +32,12 @@ function index(){
       
        <p className="m-5 max-w-lg text-xl font-semibold leading-loose text-gray-900 dark:text-white text-center mx-auto">   Grand Ballrom CDC <br></br> เลียบด่วนรามอินทรา <br></br> วันที่ 26 / 08 / 2023 </p>
       
-       <br></br>
+       <div className="bg-gradient-to-r from-yellow-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-md h-15 text-center align-middle "><p className='text-black'> Dress Code</p></div>
+
+<br></br>
+<br></br>
        <Link href="https://goo.gl/maps/gEdgGKYYx3oSPhfi8">
-       <img className=" animate-pulse h-20 mx-auto max-w-xs rounded-lg shadow-xl dark:shadow-gray-800" src="https://img.freepik.com/premium-vector/google-maps-logo_578229-304.jpg" alt="image description"/>
+       <img className=" animate-pulse h-20 mx-auto max-w-xs rounded-lg shadow-xl dark:shadow-gray-800 " src="https://img.freepik.com/premium-vector/google-maps-logo_578229-304.jpg" alt="image description"/>
        </Link>
       </div>
     </motion.div>
--- pages/schedule/index.js
+++ pages/schedule/index.js
@@ -51,7 +51,14 @@ function index(){
         <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">งานสมรสเช้า<span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">26/08/2023</span></h3>
         <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">14.29-15.39</time>
         <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">พิธีแห่ขันหมาก สู่ขอเจ้าสาวและรับตัวเจ้าสาว ฝ่ายหญิงกั้นประตูจำนวน 9 ประตู</p>
-        <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"><svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clipRule="evenodd"></path></svg> สถานที่</a>
+
+        <Link href="https://goo.gl/maps/gEdgGKYYx3oSPhfi8">
+        <a  className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+          <svg className="w-4 h-4 mr-2" strokeWidth="1.5"  fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"></path>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"></path>   </svg> 
+            สถานที่</a>
+            </Link>
     </li>
     <li className="mb-10 ml-6">
         <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
@@ -78,8 +85,7 @@ function index(){
 
       </div>
     </motion.div>
-
-
+    
 
 
 
