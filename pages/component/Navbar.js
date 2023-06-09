534294d5025aa7e6a6748544e9bcb456258a41e1
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-06-09 21:29:19 +0700

    Add files via upload

--- pages/component/Navbar.js
+++ pages/component/Navbar.js
@@ -6,22 +6,22 @@ export default function Navbar() {
 
 
 
-    <header className=' w-full h-16  border-gray-200 dark:bg-gray-800 drop-shadow-2xl z-40
+    <header className=' w-full h-16  border-gray-200 dark:bg-white drop-shadow-2xl z-40
 
  fixed  text-white text-2xl'>
 
     
-    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5  dark:bg-gray-800 ">
+    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5  dark:bg-white ">
  
         <div className="flex flex-wrap justify-between items-center  mx-auto max-w-screen-xl">
            
             <a className="flex items-center">
                 <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                <span className="self-center text-xl text-black font-semibold whitespace-nowrap dark:text-white">PB wedding</span>
+                <span className="self-center text-xl text-black font-semibold whitespace-nowrap dark:text-black">PB wedding</span>
             </a>
             <div className="flex items-center lg:order-2">
-            <a className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"></a>
-            <a  className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"></a>
+            <a className="text-gray-800 dark:text-black hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"></a>
+            <a  className="text-gray-800 dark:text-black hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"></a>
           
          
 
