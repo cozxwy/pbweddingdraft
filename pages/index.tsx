96dffd780cbbf96272f9f1088c722e9ac63be7c8
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-06-09 21:10:09 +0700

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -101,12 +101,12 @@ const Home: NextPage = () => {
 
 
                          {/* Landing pages */} 
-            <div className="snap-start max-w-5xl  mx-auto bg-scroll bg-auto bg-center bg-no-repeat bg-white dark:bg-white h-[844px] text-black dark:text-black">
+            <div className="snap-start max-w-5xl   mx-auto bg-scroll bg-auto bg-center bg-no-repeat bg-white dark:bg-white h-[844px] text-black dark:text-black">
                 
                
             <br></br><br></br><br></br><br></br><br></br><br></br>
 
-            <div className=" rounded-t-full shadow-2xl shadow-black">
+            <div className=" rounded-t-full shadow-2xl shadow-black h-full">
             <br></br><br></br>
             <AnimatePresence>
             <motion.div
@@ -304,29 +304,31 @@ const Home: NextPage = () => {
                     
                     <div className='pt-4  rounded-2xl bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-pink-500 via-red-500 to-yellow-500'> 
                     <br></br>
-                    hashtag <br></br> <p className='text-2xl'>#primoob</p> <br></br> <br></br>
+                    hashtag <br></br> <p className='text-2xl'># PRIMOOB</p> <br></br> <br></br>
                     </div>
 
                     <br></br>
                     <div className='space-x-6 flex flex-row mx-auto  my-auto text-black dark:text-white rounded-lg  '>
                     <div className="basis-1/2 text-center  bg-green-500	  drop-shadow-xl rounded-2xl  text-white dark:text-white h-16 w-15">
                    
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6 mx-auto my-auto mt-5">
+                    <Link href="tel:0959508301">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6 mx-auto my-auto mt-3">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                         </svg>
-                     
 
+                        Boom
+                    </Link>
 
                     </div>
 
                     <div className="basis-1/2 text-center  bg-green-500	  drop-shadow-xl rounded-2xl  text-white dark:text-white h-16 w-15">
-                   
-                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6 mx-auto my-auto mt-5">
+                    <Link href="tel:0839128184">
+                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6 mx-auto my-auto mt-3">
                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                        </svg>
                     
-
-
+                        Pim
+                   </Link>
                    </div>
                    
                       
